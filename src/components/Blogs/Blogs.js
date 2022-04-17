478eb4b329727c07e6de7b7fde1d578f79138b3c
Kanij Fatema <kfatema991@gmail.com>
2022-04-18 04:45:53 +0600

    component commit

---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h3>Questions & Answers</h3>
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAxOyV_ZuIS2akOxsnN7LL-YXwsljhoFHg",
+    authDomain: "independent-tour-guide-1b1b5.firebaseapp.com",
+    projectId: "independent-tour-guide-1b1b5",
+    storageBucket: "independent-tour-guide-1b1b5.appspot.com",
+    messagingSenderId: "468914885797",
+    appId: "1:468914885797:web:1fe90476cbb7d1f34ae94c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
